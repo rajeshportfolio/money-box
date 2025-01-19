a2ccf59539956932c59795192b293e4e115dbe6a
rajeshportfolio <rk044478@gmail.com>
2025-01-19 15:27:15 +0530

    Full Code here

---
+++ js/main.js
@@ -0,0 +1,107 @@
+$(document).ready(function () {
+    $(".owl-carousel-banner").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: true,
+        items: 1, 
+        autoplay: true,
+        autoplayTimeout: 2500,
+    });
+
+    $(".owl-carousel-earn-while").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: true,
+        items: 1, 
+        autoplay: true,
+        autoplayTimeout: 2500,
+    });
+
+    $(".owl-carousel-Featured").owlCarousel({
+        loop: true,
+        margin: 16,
+        nav: false,
+        dots: false,
+        items: 5,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsive : {
+            0 : {
+                items: 2,
+            },
+            575 : {
+                items: 3,
+            },
+            767 : {
+                items: 4,
+            },
+            1200 : {
+                items: 5,
+            }
+        }
+    });
+
+    $(".owl-carousel-popularbrands").owlCarousel({
+        loop: true,
+        margin: 16,
+        nav: false,
+        dots: false,
+        items: 5,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsive : {
+            0 : {
+                items: 1,
+            },
+            575 : {
+                items: 2,
+            }
+        }
+    });
+
+    $(".owl-carousel-TopDeals").owlCarousel({
+        loop: true,
+        margin: 16,
+        center: true,
+        nav: true,
+        dots: false,
+        items: 6,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        responsive : {
+            0 : {
+                items: 1,
+            },
+            435 : {
+                items: 2,
+            },
+            991 : {
+                items: 4,
+            }
+        }
+    });
+
+    $(".owl-carousel-feedback").owlCarousel({
+        loop: true,
+        center: true,
+        margin: 25,
+        nav: false,
+        dots: false,
+        items: 4,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsive : {
+            0 : {
+                items: 1,
+            },
+            767 : {
+                items: 2,
+            },
+            1200 : {
+                items: 4,
+            }
+        }
+    });
+});
